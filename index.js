9742923a8dfb1985a6c0dc53c53d7413813d5834
Vadik-K <98230417+Vadik-K@users.noreply.github.com>
2022-03-13 23:50:15 +0500

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,12 @@
+"infoURL": "https://thundercore.com",
+  "shortName": "TST",
+  "chainId": 18,
+  "networkId": 18
+} 
+  "networkId": 18,
+  "explorers": [{
+    "name": "ThundercoreTestNetScanner",
+    "url": "https://scan-testnet.thundercore.com",
+    "standard": "none"
+  }]
+}
